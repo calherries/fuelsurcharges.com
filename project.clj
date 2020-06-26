8229084ce7bdf6c71ba1ead76e0a6a14402acbb9
Callum Herries <callum.herries@uber.com>
2020-06-26 17:42:24 +0200

    Merged shadow-cljs.edn into project.clj, unsuccessfully

--- project.clj
+++ project.clj
@@ -72,6 +72,24 @@
              [react-dom "16.8.6"]]
   :npm-dev-deps [[xmlhttprequest "1.8.0"]]
 
+  :shadow-cljs { :nrepl {:port 7002}
+                :builds {:app  {:target     :browser
+                                :output-dir "target/cljsbuild/public/js"
+                                :asset-path "/js"
+                                :modules    {:app {:entries [fuelsurcharges.core]}}
+                                :devtools   {:watch-dir "resources/public"
+                                             :preloads  [devtools.preload
+                                                         day8.re-frame-10x.preload
+                                                         shadow.remote.runtime.cljs.browser]}
+                                :dev        {:compiler-options
+                                             {:optimization    :none
+                                              :closure-defines {re_frame.trace.trace_enabled?                true
+                                                                "re_frame.trace.trace_enabled_QMARK_"        true
+                                                                "day8.re_frame.tracing.trace_enabled_QMARK_" true
+                                                                "goog.DEBUG"                                 true}}}}
+                         :test {:target    :node-test
+                                :output-to "target/test/test.js"
+                                :autorun   true}}}
   :profiles
   {:uberjar {:omit-source    true
              :prep-tasks     ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
