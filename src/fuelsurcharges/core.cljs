b900577598f404c0d8786220ae821579d2574791
Callum Herries <callum.herries@uber.com>
2020-06-27 16:02:27 +0200

    Add svg chart and format header

--- src/fuelsurcharges/core.cljs
+++ src/fuelsurcharges/core.cljs
@@ -13,6 +13,8 @@
    [oz.core :as oz]
    [reitit.frontend.easy :as rfe]
    [clojure.string :as string]
+   [goog.string :as gstring]
+   [goog.string.format]
    [mount.core :as mount])
   (:import goog.History))
 
@@ -24,20 +26,76 @@
    :mark     "line"
    :width    800})
 
+(def uniqkey (atom 0))
+(defn gen-key []
+  (let [res (swap! uniqkey inc)]
+    res))
+
+(def points
+  "0,120 20,60 40,80 60,20")
+(gen-key)
+
+(defn svg
+  [[minx miny user-width user-height :as dims] width height contents]
+  [:svg {:xmlns   "http://www.w3.org/2000/svg" :version "1.1"
+         :viewBox (apply gstring/format "%f %f %f %f" (map double dims))
+         :width   (gstring/format "%dpx" width)
+         :height  (gstring/format "%dpx" height)}
+   contents])
+
+(defn css
+  [m]
+  (string/join (for [[k v] m]
+                 (gstring/format "%s:%s;" (name k) (str v)))))
+
+(defn set-style
+  [attrs]
+  (cond-> attrs
+    (map? (:style attrs))
+    (update-in [:style] css)))
+
+(defn elem
+  ([tagname attrs]
+   [tagname (set-style attrs)])
+  ([tagname attrs contents]
+   [tagname (set-style attrs) contents]))
+
+(defn ^:private points-str
+  [sep points]
+  (string/join sep (for [[x y] points] (str (double x) \, (double y)))))
+
+(defn polyline
+  ([points] (polyline points {}))
+  ([points attrs]
+   (elem :polyline (assoc attrs :points (points-str " " points)))))
+
+
 (defn home []
   (let [loading?   @(rf/subscribe [:markets/loading?])
-        market-ids @(rf/subscribe [:markets/list])
-        ]
-    [:div.content>div.columns.is-centered>div.column.is-two-thirds
-     (if loading?
-       [:h3 "Loadin markets"]
-       [:div
-        [:h3 "loaded!"]
-        [:p (str market-ids)]
+        market-ids @(rf/subscribe [:markets/list])]
+    [:div.page.w-screen
+     [:div.header.m-auto.max-w-screen-xl.p-3
+      [:h1.text-2xl.font-bold.text-center "FuelSurcharges.com"]]
+     [:div.content.m-auto.max-w-screen-xl
+      (if loading?
+        [:h3 "Loadin markets"]
         [:div
-         (for [id market-ids]
-           (let [prices @(rf/subscribe [:markets/prices-list-by-id id])]
-             [(oz/vega-lite (line-plot prices))]))]])]))
+         [:h3 "loaded!"]
+         [:p (str market-ids)]
+         [:div
+          (let [prices @(rf/subscribe [:markets/prices-list-by-id 1])
+                height 500
+                width  1000
+                xs     (map #(* % (/ width (count prices))) (range))
+                ys     (->> prices
+                            (map :price)
+                            (map #(float (/ % height)))
+                            (map vector xs))])
+          [:svg {:viewBox [0 0 500 100]}
+           [:polyline {:points points :stroke "black" :fill "none"}]]
+          (for [id market-ids]
+            (let [prices @(rf/subscribe [:markets/prices-list-by-id id])]
+              [(oz/vega-lite (line-plot prices))]))]])]]))
 
 ;; -------------------------
 ;; Initialize app
